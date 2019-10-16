da3f5543b683beb699e6ba6338b01c28031bc155
edras vallecillo <samir.vallecillo@outlook.com>
2019-10-16 23:53:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,6 +65,15 @@ class Student
     DB[:conn].execute(sql, x)
   end	
   
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade = 10 ORDER BY students.id LIMIT ?
+    SQL
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
+end
+  
   def self.first_student_in_grade_10
     sql = <<-SQL
       SELECT * FROM students
